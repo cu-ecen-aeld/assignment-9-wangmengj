ad9e9bc5c71fdae2501ff8d5cb76dfb73ee4ca55
wangmengj <wangmengj@gmail.com>
2024-06-01 10:07:32 -0700

    another correction for assignment8 buildroot

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'b18ee6f0d229cec29505fafa8ef4187f50fb7e26'
+AESD_ASSIGNMENTS_VERSION = 'd6322f46a55c567a8e03946f1b801b90ca14a2df'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
